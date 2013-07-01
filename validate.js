c537fb633db5947c349f48eb02993a7c5185a1bc
Peter Shugar <pwshugar@gmail.com>
2013-06-30 22:52:03 -0700

    added checkboxes

--- validate.js
+++ validate.js
@@ -3,22 +3,16 @@ $(function(){
   var formInfo = {}
 
   var formHandler = function(event){
-    console['log']("submitted");
-
-    for (var i = 0; i < $("input").length; i++){
-    	formInfo[$("input")[i].id] = $("input")[i].value;
+    for (var i = 0; i < $(".inp").length; i++){
+    	formInfo[$(".inp")[i].id] = $(".inp")[i].value;
     }
-
 	for (k in formInfo){
 		$("." + k).text(formInfo[k]);
 		$("#" + k)[0].value = "";
 	}
-
-	$("#submit")[0].value = "Submit";
-
-
     event['preventDefault']();
   };
+
 
   $("form")['submit'](formHandler);
 
