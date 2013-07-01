0c854007c0777c384d56d7538c1f8cf1b43851d8
Peter Shugar <pwshugar@gmail.com>
2013-07-01 10:01:32 -0700

    regex not fully working

--- validate.js
+++ validate.js
@@ -2,27 +2,39 @@ $(function(){
   var formInfo = {};
   
   var formHandler = function(event){
-  	$("#rd").text("");
+
+    for (var j = 0; j < $(".inp").length; j++){
+    	formInfo[$(".inp")[j].id] = $(".inp")[j].value;
+    }
+    if (!checker()){
+  		return;
+  	}
+	for (k in formInfo){
+		$("." + k).text(formInfo[k]);
+		$("#" + k)[0].value = "";
+	}
+	$("#rd").text("");
     for (var i = 0; i < $(".rd").length; i++){
     	if ($(".rd")[i].checked){
     		$("#rd").text($(".rd")[i].value);
     		$(".rd")[i].checked = false;
     	}
     }
-    for (var j = 0; j < $(".inp").length; j++){
-    	formInfo[$(".inp")[j].id] = $(".inp")[j].value;
-    }
-	for (k in formInfo){
-		$("." + k).text(formInfo[k]);
-		$("#" + k)[0].value = "";
-	}
-
     event['preventDefault']();
   };
 
   var checker = function(){
+  	// debugger;
   	var reCard = /\d{16}/;
-  	if (reCard.exec())
+  	var reCcv = /\d{4}/;
+  	if (!reCard.exec(formInfo['card'])){
+  		alert("Please enter a 16 digit Credit Card number.");
+  		return false;
+  	}
+  	if ()
+
+
+  	return true;
   };
 
   $("form")['submit'](formHandler);
