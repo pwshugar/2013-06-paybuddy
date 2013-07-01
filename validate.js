a17323585c632f1e49f2a8efdb0bce369d04dd92
Peter Shugar <pwshugar@gmail.com>
2013-06-30 23:56:07 -0700

    radio boxes working

--- validate.js
+++ validate.js
@@ -1,18 +1,25 @@
 $(function(){
 
-  var formInfo = {}
-
+  var formInfo = {};
   var formHandler = function(event){
-    for (var i = 0; i < $(".inp").length; i++){
-    	formInfo[$(".inp")[i].id] = $(".inp")[i].value;
+  	console.log($(".rd"));
+  	$("#rd").text("");
+    for (var i = 0; i < $(".rd").length; i++){
+    	if ($(".rd")[i].checked){
+    		$("#rd").text($(".rd")[i].value);
+    		$(".rd")[i].checked = false;
+    	}
+    }
+    for (var j = 0; j < $(".inp").length; j++){
+    	formInfo[$(".inp")[j].id] = $(".inp")[j].value;
     }
 	for (k in formInfo){
 		$("." + k).text(formInfo[k]);
 		$("#" + k)[0].value = "";
 	}
+
     event['preventDefault']();
   };
-
 
   $("form")['submit'](formHandler);
 
